caa3921189c001c55b9e1a497fdf9d169ab6733d
Javier Pepe <rootsco@users.noreply.github.com>
2018-06-26 13:02:46 -0300

    add additional properties to user model

--- server/src/models/UserModel.ts
+++ server/src/models/UserModel.ts
@@ -5,11 +5,15 @@ export interface IUserModel {
   id?: string;
   email: string;
   name: string;
+  mobile?: string;
+  skypeId?: string;
 }
 
 export class UserFormatter extends BaseFormatter implements IUserModel {
   public email: string = undefined;
   public name: string = undefined;
+  public mobile: string = undefined;
+  public skypeId: string = undefined; 
 
   constructor(args: any) {
     super();
--- server/src/tests/data/models.ts
+++ server/src/tests/data/models.ts
@@ -8,5 +8,7 @@ export const generateMockUUID = (): string => {
 
 export const generateUserModel = (): Models.IUserModel => ({
   email: generate(20),
-  name: generate(20)
+  name: generate(20),
+  mobile: generate(20),
+  skypeId: generate(20)
 });
