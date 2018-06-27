0906fd96cc7624f9cd83bf0301317e0e157d3d09
Emanuel Pereyra <emanuelmetal@gmail.com>
2018-06-27 14:08:32 -0300

    Add action buttons (#5)
    * Added buttons to Add, Edit & Delete Users
    
    Interactions added to enable and disable buttons upon row selection
    
    * Address code review comment

--- webapp/app/components/pages/home/HomePage.jsx
+++ webapp/app/components/pages/home/HomePage.jsx
@@ -2,35 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Button, Col, Row } from 'reactstrap';
 import BootstrapTable from 'react-bootstrap-table-next';
 import * as usersActions from '../../../actions/usersActios';
 import Footer from './../partials/footer/Footer';
 import Header from './../partials/header/Header';
+import './HomePage.css';
 
 export class HomePage extends React.Component {
   constructor(props, context) {
     super(props, context);
 
     this.state = {
-      name: '',
-      email: '',
-      skypeId: '',
-      mobile: ''
+      selectedUser: []
     };
 
-    this.handleOnChange = this.handleOnChange.bind(this);
-    // this.handleOnSubmit = this.handleOnSubmit.bind(this);
+    this.setSelectedRow = this.setSelectedRow.bind(this);
   }
 
   componentDidMount() {
     this.props.actions.getUsers();
   }
 
-  handleOnChange(e) {
+  setSelectedRow(row) {
     this.setState({
-      [e.target.name]: e.target.value
+      selectedUser: [row.id]
     });
   }
+
   render() {
     const columns = [{
       dataField: 'name',
@@ -46,13 +45,25 @@ export class HomePage extends React.Component {
     const selectRow = {
       mode: 'radio',
       clickToSelect: true,
-      bgColor: '#c8e6c9'
+      bgColor: '#c8e6c9',
+      selected: this.state.selectedUser,
+      onSelect: this.setSelectedRow
     };
     
     return (
       <div>
         <Header />
         <div className="container">
+          <Row>
+            <Col md="8">
+              <h4>Users List</h4>
+            </Col>
+            <Col md="4">
+              <Button color="primary">Add</Button>{' '}
+              <Button color="info" disabled={ this.state.selectedUser.length === 0 }>Edit</Button>{' '}
+              <Button color="danger" disabled={ this.state.selectedUser.length === 0 }>Delete</Button>{' '}
+            </Col>
+          </Row>
           <BootstrapTable
             keyField='id'
             data={ this.props.users }
