118b3e05843aec336279fe90c7ea5271f8db8d29
Javier Pepe <rootsco@users.noreply.github.com>
2018-07-02 16:46:53 -0300

    added coverage entry

--- webapp/jest.config.js
+++ webapp/jest.config.js
@@ -5,4 +5,9 @@ module.exports = {
   'moduleNameMapper': {
     '\\.(css|less)$': '<rootDir>/__mocks__/style-mock.js'
   },
+  'collectCoverageFrom': [
+    '**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**'
+  ]
 };
