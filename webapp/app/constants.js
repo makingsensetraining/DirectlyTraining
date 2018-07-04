3ea070ad364ca968b73cd0339343f30984d7a1e1
Javier Pepe <rootsco@users.noreply.github.com>
2018-07-04 13:29:40 -0300

    add users endpoint api

--- webapp/app/constants.js
+++ webapp/app/constants.js
@@ -1,2 +1,3 @@
 export const DEFAULT_API_BASE_URL = 'http://localhost:3030/service';
+export const DEFAULT_API_USERS_ENDPOINT = 'http://localhost:3030/service/users';
 export const EMAIL_REGEXP = /([\w.\-_]+)?\w+@[\w-_]+(\.\w+){1,}/ig;
