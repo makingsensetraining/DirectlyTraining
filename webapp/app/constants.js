fd178acda12c1f441751f76055a313dee330301c
Javier Pepe <rootsco@users.noreply.github.com>
2018-07-04 16:11:53 -0300

    disable linter for email regexp

--- webapp/app/constants.js
+++ webapp/app/constants.js
@@ -2,4 +2,5 @@ export const DEFAULT_API_BASE_URL = 'http://localhost:3030/service';
 export const DEFAULT_API_USERS_ENDPOINT = 'http://localhost:3030/service/users';
 
 // from http://emailregex.com
+/* eslint-disable-next-line */
 export const EMAIL_REGEXP = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
