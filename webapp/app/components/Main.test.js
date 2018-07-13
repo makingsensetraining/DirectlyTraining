ca01258094cceee82366cc35fcd32e09a3f7811c
Ivan Scoles <iscoles@makingsense.com>
2018-07-13 00:11:04 -0300

    Deleted context props from test main component.

--- webapp/app/components/Main.test.js
+++ webapp/app/components/Main.test.js
@@ -5,26 +5,8 @@ import Main from './Main';
 describe('<Main /> component', () => {
 
   it('renders itself', () => {
-    const mockContext = {
-      auth: {
-        authenticating: false,
-        isAuthenticated: false,
-        error: false,
-        errorMessage: null,
-        user: null
-      },
-      routing: null,
-      users: {
-        data: [],
-        selectedUser: {},
-        fetch: {
-          loading: false,
-          error: null
-        }
-      }
-    };
     // Arrange
-    const wrapper = shallow(<Main context={mockContext} />);
+    const wrapper = shallow(<Main />);
 
     // Assert
     expect(wrapper.find('Switch')).toHaveLength(1);
