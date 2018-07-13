3c0dabb95f3c6d75d47f6e3da52d9514b93e06e2
Ivan Scoles <iscoles@makingsense.com>
2018-07-13 00:14:47 -0300

    Deleted context props from test login page component.

--- webapp/app/components/Main.test.js
+++ webapp/app/components/Main.test.js
@@ -5,10 +5,8 @@ import Main from './Main';
 describe('<Main /> component', () => {
 
   it('renders itself', () => {
-    // Arrange
     const wrapper = shallow(<Main />);
 
-    // Assert
     expect(wrapper.find('Switch')).toHaveLength(1);
     expect(wrapper.find('Route')).toHaveLength(3);
   });
--- webapp/app/components/pages/login/LoginPage.test.js
+++ webapp/app/components/pages/login/LoginPage.test.js
@@ -6,50 +6,10 @@ function setup(props) {
   return shallow(<LoginPage {...props} />);
 }
 describe('<LoginPage /> component', () => {
-  let mockContext;
-
-  beforeEach(() => {
-    mockContext = {
-      auth: {
-        authenticating: false,
-        isAuthenticated: false,
-        error: false,
-        errorMessage: null,
-        user: null
-      },
-      routing: null,
-      users: {
-        data: [],
-        selectedUser: {},
-        fetch: {
-          loading: false,
-          error: null
-        }
-      }
-    };
-  });
-
   it('renders itself', () => {
-    const wrapper = setup({
-      context: mockContext
-    });
+    const wrapper = setup({});
 
     expect(wrapper.find('section')).toHaveLength(1);
     expect(wrapper.find('LoginForm')).toHaveLength(1);
   });
-  /**
-  it('should handle form submit itself', () => {
-    const login = jest.fn();
-    const wrapper = setup({
-      actions: {
-        login
-      }
-    });
-    const form = wrapper.find('LoginForm');
-
-    form.simulate('submit');
-
-    expect(login).toHaveBeenCalledTimes(1);
-  });
-  **/
 });
