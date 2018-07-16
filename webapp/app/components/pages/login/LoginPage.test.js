bf62c07c52f49e3f1931393251c60ce2ec60c6b6
Ivan Scoles <iscoles@makingsense.com>
2018-07-16 15:50:06 -0300

    Adding coverage to home, login pages with context api. Added sinon to webapp package.json.

--- webapp/app/components/pages/login/LoginPage.test.js
+++ webapp/app/components/pages/login/LoginPage.test.js
@@ -1,20 +1,41 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import LoginPage from './LoginPage';
+import { shallow } from 'enzyme';
 
 function setup(props) {
   return shallow(<LoginPage {...props} />);
 }
+
 describe('<LoginPage /> component', () => {
-  const mockAuthContext = {
-    login: jest.fn
-  };
+  let defaultProps;
+
+  beforeEach(() => {
+    defaultProps = {
+      authContext: {
+        login: jest.fn
+      }
+    };
+  });
+
   it('renders itself', () => {
-    const wrapper = setup({
-      authContext: mockAuthContext
-    });
+    const wrapper = setup(defaultProps);
 
     expect(wrapper.find('section')).toHaveLength(1);
     expect(wrapper.find('LoginForm')).toHaveLength(1);
+    expect(wrapper.find('LoginForm').props()).toMatchObject({
+      onSubmit: wrapper.instance().handleOnSubmit
+    });
+  });
+
+  it('should call auth context login when form is submitted', () => {
+    const mockLogin = jest.fn();
+    defaultProps.authContext = {
+      login: mockLogin
+    };
+    const wrapper = setup(defaultProps);
+
+    wrapper.instance().handleOnSubmit('john', 'doe');
+
+    expect(mockLogin).toBeCalledWith('john', 'doe');
   });
 });
