08ea0f843af7e50726328dab0cecc67094b09aea
Ivan Scoles <iscoles@makingsense.com>
2018-07-23 10:22:39 -0300

    Adding more coverage to LoginFormMain and LoginFormTop components.

--- webapp/app/components/pages/login/LoginFormMain.test.js
+++ webapp/app/components/pages/login/LoginFormMain.test.js
@@ -4,31 +4,37 @@ import configureMockStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 import { mount } from 'enzyme';
 
-const mockStore = configureMockStore();
-const store = mockStore({});
-
-function setup(props) {
-  return mount(
-    <Provider store={store}>
-      <LoginFormMain {...props} />
-    </Provider>
-  );
+function setup(store) {
+  return mount(<LoginFormMain store={store} />);
 }
 
 describe('<LoginFormMain /> component', () => {
+  const mockStore = configureMockStore();
+  let store;
+  beforeEach(function () {
+    store = mockStore({});
+  });
+
   it('renders itself', () => {
-    const wrapper = setup({
-      auth: {
-        error: false,
-        errorMessage: null
-      }
-    });
+    const wrapper = setup(store);
 
     expect(wrapper.find('form')).toHaveLength(1);
     expect(wrapper.find('FormGroup')).toHaveLength(2);
     expect(wrapper.find('Label')).toHaveLength(2);
     expect(wrapper.find('Input')).toHaveLength(2);
     expect(wrapper.find('Button')).toHaveLength(1);
-    expect(wrapper.find('.alert alert-danger')).toHaveLength(0);
+    expect(wrapper.find('.alert')).toHaveLength(0);
+  });
+
+  it('should render auth error message', () => {
+    store = mockStore({
+      auth: {
+        error: true,
+        errorMessage: 'Invalid credentials.'
+      }
+    });
+    const wrapper = setup(store);
+
+    expect(wrapper.find('.alert .alert-danger')).toHaveLength(1);
   });
 });
--- webapp/app/components/pages/login/LoginFormTop.test.js
+++ webapp/app/components/pages/login/LoginFormTop.test.js
@@ -4,31 +4,38 @@ import configureMockStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 import { mount } from 'enzyme';
 
-const mockStore = configureMockStore();
-const store = mockStore({});
-
-function setup(props) {
-  return mount(
-    <Provider store={store}>
-      <LoginFormTop {...props} />
-    </Provider>
-  );
+function setup(store) {
+  return mount(<LoginFormTop store={store} />);
 }
 
 describe('<LoginFormTop /> component', () => {
+  const mockStore = configureMockStore();
+  let store;
+
+  beforeEach(function () {
+    store = mockStore({});
+  });
+
   it('renders itself', () => {
-    const wrapper = setup({
-      auth: {
-        error: false,
-        errorMessage: null
-      }
-    });
+    const wrapper = setup(store);
 
     expect(wrapper.find('form')).toHaveLength(1);
     expect(wrapper.find('FormGroup')).toHaveLength(2);
     expect(wrapper.find('Label')).toHaveLength(2);
     expect(wrapper.find('Input')).toHaveLength(2);
     expect(wrapper.find('Button')).toHaveLength(1);
-    expect(wrapper.find('.alert alert-danger')).toHaveLength(0);
+    expect(wrapper.find('.alert .alert-danger')).toHaveLength(0);
+  });
+
+  it('should render auth error message', () => {
+    store = mockStore({
+      auth: {
+        error: true,
+        errorMessage: 'Invalid credentials.'
+      }
+    });
+    const wrapper = setup(store);
+
+    expect(wrapper.find('.alert .alert-danger')).toHaveLength(1);
   });
 });
